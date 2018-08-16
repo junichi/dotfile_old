721c1f5fc22127992bc90368878d38d54ec8b721
junichi <nakajun425@gmail.com>
2018-08-17 02:59:47 +0900

    removed unnecessary configs

--- etc/init/windows/chocolatey.bat
+++
@@ -1,4 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-xcopy H:\dotfiles\etc\init\windows\chocolaty\packages.config %homepath%\inst\
-cinst %homepath%\inst\packages.config -y
-rd /s /q %homepath%\inst
